b141bca7ce98c644430557b35338410978a2b850
Christian Lechner <lechnerc77@users.noreply.github.com>
2025-01-20 08:37:23 +0100

    fix: provisioning (#4)
    Co-authored-by: Christian Lechner <22294087+lechnerc77@users.noreply.github.com>

--- stacks/btp_test/_terramate_generated_outputs.tf
+++
@@ -1,6 +0,0 @@
-// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
-
-output "cf_landscape_label" {
-  description = "The Cloud Foundry landscape label"
-  value       = btp_subaccount_environment_instance.cloudfoundry.landscape_label
-}
